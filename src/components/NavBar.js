4fcabe2f244d4afc12c4f70f2ace039d851fc1da
wachuka7 <rachaelwachuka7@gmail.com>
2024-02-01 01:00:39 +0300

    Lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,10 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return <nav>NavBar
+    <a href="#hme"></a>
+    <a href="#about"></a>
+  </nav>;
 }
 
 export default NavBar;
--- src/data/data.js
+++ src/data/data.js
@@ -4,4 +4,4 @@ const city = "New York";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
-export { name, city, image };
+export { name, city, image};
